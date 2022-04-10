8661f2ac9c7c48d49c614272f0cdf7bab5e3deb4
sisingla <sisingla@deloitte.com>
2022-04-10 21:04:54 +0530

    feat: simplify implementation

--- csharp-training/csharp-training/csharp-training/Program.cs
+++ csharp-training/csharp-training/csharp-training/Program.cs
@@ -7,15 +7,27 @@ namespace CSharpTraining
         public static void Main(string[] args)
         {
             //DateTime and Timespan Block
+            dateTimeChapter();
+
+            //String and String Builder
+            stringChapter();
+        }
+
+        public static void dateTimeChapter()
+        {
             DateTime now = DateTime.UtcNow;
             DateNtimespan dateNtimespan = new DateNtimespan(now, now.AddHours(1));
             dateNtimespan.passedDuration();
             Console.WriteLine("{0} minutes passed.", dateNtimespan.passedDuration());
+        }
 
-            //String and String Builder
+        public static void stringChapter()
+        {
             StringNStringBuilder stringNStringBuilder = new StringNStringBuilder("Sinni Singla");
-            Console.WriteLine("I am '{0}'",stringNStringBuilder.getFirstName());
+            Console.WriteLine("I am '{0}'", stringNStringBuilder.getFirstName());
             stringNStringBuilder.changeName("Shanky Singla");
+            float f = 10.12f;
+            Console.WriteLine(f.ToString("C2"));
         }
     }
 }
